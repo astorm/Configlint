fef2e7cbdbbe98bb3a6e0c921bbf72b15ab93d52
Alan Storm <astorm@alanstorm.com>
2013-06-15 12:15:54 -0700

    Updating license dates/names

--- Contributedlints/Helper/ExtensionFilter.php
+++ Contributedlints/Helper/ExtensionFilter.php
@@ -1,5 +1,5 @@
 <?php
-//     Copyright (c) 2010 Kristof Ringleff
+//     Copyright (c) 2010 - 2013 Kristof Ringleff
 //     
 //     Permission is hereby granted, free of charge, to any person obtaining a copy
 //     of this software and associated documentation files (the "Software"), to deal
